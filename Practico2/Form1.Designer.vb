baca5016507c45d88ef93355fdcfc9e8d3d3a924
laran <quintanarijoan@gmail.com>
2026-08-17 18:18:55 -0300

    Agregar archivos de proyecto.

---
+++ Practico2/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        LDni = New Label()
+        LNombre = New Label()
+        LApellido = New Label()
+        LNya = New Label()
+        LModificar = New Label()
+        TDni = New TextBox()
+        TNombre = New TextBox()
+        TApellido = New TextBox()
+        BGuardar = New Button()
+        BEliminar = New Button()
+        BindingSource1 = New BindingSource(components)
+        CType(BindingSource1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' LDni
+        ' 
+        LDni.AutoSize = True
+        LDni.Location = New Point(48, 109)
+        LDni.Name = "LDni"
+        LDni.Size = New Size(27, 15)
+        LDni.TabIndex = 0
+        LDni.Text = "DNI"
+        ' 
+        ' LNombre
+        ' 
+        LNombre.AutoSize = True
+        LNombre.Location = New Point(48, 217)
+        LNombre.Name = "LNombre"
+        LNombre.Size = New Size(51, 15)
+        LNombre.TabIndex = 1
+        LNombre.Text = "Nombre"
+        ' 
+        ' LApellido
+        ' 
+        LApellido.AutoSize = True
+        LApellido.Location = New Point(48, 168)
+        LApellido.Name = "LApellido"
+        LApellido.Size = New Size(51, 15)
+        LApellido.TabIndex = 2
+        LApellido.Text = "Apellido"
+        ' 
+        ' LNya
+        ' 
+        LNya.AutoSize = True
+        LNya.Location = New Point(48, 53)
+        LNya.Name = "LNya"
+        LNya.Size = New Size(110, 15)
+        LNya.TabIndex = 3
+        LNya.Text = "Nombre y Apellido:"
+        ' 
+        ' LModificar
+        ' 
+        LModificar.AutoSize = True
+        LModificar.ForeColor = Color.Red
+        LModificar.Location = New Point(179, 53)
+        LModificar.Name = "LModificar"
+        LModificar.Size = New Size(58, 15)
+        LModificar.TabIndex = 4
+        LModificar.Text = "modificar"
+        ' 
+        ' TDni
+        ' 
+        TDni.Location = New Point(132, 106)
+        TDni.Name = "TDni"
+        TDni.Size = New Size(100, 23)
+        TDni.TabIndex = 5
+        ' 
+        ' TNombre
+        ' 
+        TNombre.Location = New Point(132, 217)
+        TNombre.Name = "TNombre"
+        TNombre.Size = New Size(100, 23)
+        TNombre.TabIndex = 6
+        ' 
+        ' TApellido
+        ' 
+        TApellido.Location = New Point(132, 160)
+        TApellido.Name = "TApellido"
+        TApellido.Size = New Size(100, 23)
+        TApellido.TabIndex = 7
+        ' 
+        ' BGuardar
+        ' 
+        BGuardar.Location = New Point(83, 287)
+        BGuardar.Name = "BGuardar"
+        BGuardar.Size = New Size(75, 23)
+        BGuardar.TabIndex = 8
+        BGuardar.Text = "&Guardar"
+        BGuardar.UseVisualStyleBackColor = True
+        ' 
+        ' BEliminar
+        ' 
+        BEliminar.Location = New Point(211, 287)
+        BEliminar.Name = "BEliminar"
+        BEliminar.Size = New Size(75, 23)
+        BEliminar.TabIndex = 9
+        BEliminar.Text = "&Eliminar"
+        BEliminar.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(384, 361)
+        Controls.Add(BEliminar)
+        Controls.Add(BGuardar)
+        Controls.Add(TApellido)
+        Controls.Add(TNombre)
+        Controls.Add(TDni)
+        Controls.Add(LModificar)
+        Controls.Add(LNya)
+        Controls.Add(LApellido)
+        Controls.Add(LNombre)
+        Controls.Add(LDni)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Pequeño Formulario"
+        CType(BindingSource1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents LDni As Label
+    Friend WithEvents LNombre As Label
+    Friend WithEvents LApellido As Label
+    Friend WithEvents LNya As Label
+    Friend WithEvents LModificar As Label
+    Friend WithEvents TDni As TextBox
+    Friend WithEvents TNombre As TextBox
+    Friend WithEvents TApellido As TextBox
+    Friend WithEvents BGuardar As Button
+    Friend WithEvents BEliminar As Button
+    Friend WithEvents BindingSource1 As BindingSource
+
+End Class
---
+++ Practico2/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+    Private Sub LDni_Click(sender As Object, e As EventArgs) Handles LDni.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    ' Validar solo números en TDni
+    Private Sub TDni_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TDni.KeyPress
+        If Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True ' Cancela el caracter ingresado
+        End If
+    End Sub
+
+    ' Validar solo letras en TApellido
+    Private Sub TApellido_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TApellido.KeyPress
+        If Not Char.IsLetter(e.KeyChar) And Not Char.IsControl(e.KeyChar) And Not Char.IsWhiteSpace(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    ' Validar solo letras en TNombre
+    Private Sub TNombre_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TNombre.KeyPress
+        If Not Char.IsLetter(e.KeyChar) And Not Char.IsControl(e.KeyChar) And Not Char.IsWhiteSpace(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub BGuardar_Click(sender As Object, e As EventArgs) Handles BGuardar.Click
+        ' 7) Validar si algún campo está vacío con el operador Or
+        If TDni.Text.Trim() = "" Or TApellido.Text.Trim() = "" Or TNombre.Text.Trim() = "" Then
+            MsgBox("Debe Completar todos los campos", MsgBoxStyle.Critical, "Error")
+            Exit Sub
+        End If
+
+        ' 8) crear una variable
+        Dim ask As MsgBoxResult
+
+        '9) preguntar confirmacion
+        ask = MsgBox("Seguro que desea insertar un nuevo Cliente?", MsgBoxStyle.YesNo Or MsgBoxStyle.Question, "Confirmar Insercion")
+
+        If ask = MsgBoxResult.Yes Then
+            ' 6) Si pasó la validación, modificar el label
+            LModificar.Text = TNombre.Text & " " & TApellido.Text
+            MsgBox("El cliente: " & LModificar.Text & " se inserto correctamente", MsgBoxStyle.Information, "Guardar")
+        End If
+
+
+    End Sub
+
+    Private Sub BEliminar_Click(sender As Object, e As EventArgs) Handles BEliminar.Click
+        If TDni.Text.Trim() = "" Or TApellido.Text.Trim() = "" Or TNombre.Text.Trim() = "" Then
+            MsgBox("Debe Completar todos los campos", MsgBoxStyle.Critical, "Error")
+            Exit Sub
+        End If
+
+        LModificar.Text = TNombre.Text & " " & TApellido.Text
+        Dim ask As MsgBoxResult
+        ask = MsgBox("Seguro que desea eliminar el Cliente:" & LModificar.Text, MsgBoxStyle.Exclamation Or MsgBoxStyle.YesNo Or MsgBoxStyle.DefaultButton2, "Confirmar Eliminacion")
+
+        If ask = MsgBoxResult.Yes Then
+
+
+            MsgBox("El cliente: " & LModificar.Text & " se eliminó correctamente", MsgBoxStyle.Information, "Eliminar")
+            LModificar.Text = "modificar"
+            TDni.Clear()
+            TApellido.Clear()
+            TNombre.Clear()
+
+        End If
+
+    End Sub
+
+End Class
